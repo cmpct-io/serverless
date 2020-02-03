8e203fd82ddda2fc3cefc290e4854beea1329de2
Thomas William McClean <spam@tommcclean.me>
2020-02-03 21:43:08 +0000

    Ability to append page titles to links

---
+++ src/Compact.Functions/AzureStorageManager.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Compact.Functions
+{
+    public interface IAzureStorageManager
+    {
+        Task<string> StoreObject(string containerName, string fileName, object obj);
+    }
+
+    public class AzureStorageManager : IAzureStorageManager
+    {
+        private readonly string _storageConnectionString;
+
+        public AzureStorageManager(string storageConnectionString)
+        {
+            _storageConnectionString = storageConnectionString;
+        }
+
+        public async Task<string> StoreObject(string containerName, string fileName, object obj)
+        {
+            var storageAccount = ConnectStorageAccount();
+            var cloudBlobContainer = FetchBlobContainer(storageAccount, containerName);
+            await cloudBlobContainer.CreateIfNotExistsAsync();
+
+            BlobContainerPermissions permissions = new BlobContainerPermissions
+            {
+                PublicAccess = BlobContainerPublicAccessType.Blob,
+            };
+
+            await cloudBlobContainer.SetPermissionsAsync(permissions);
+
+            CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(fileName);
+
+            var jsonFileContent = JsonConvert.SerializeObject(obj);
+
+            await cloudBlockBlob.UploadTextAsync(jsonFileContent);
+
+            return cloudBlobContainer.Uri.AbsoluteUri + "/" + fileName;
+        }
+
+        private CloudStorageAccount ConnectStorageAccount()
+        {
+            if (CloudStorageAccount.TryParse(_storageConnectionString, out CloudStorageAccount storageAccount))
+            {
+                return storageAccount;
+            }
+            else
+            {
+                throw new WebException("Unable to connect to storage account");
+            }
+        }
+
+        private static CloudBlobContainer FetchBlobContainer(CloudStorageAccount storageAccount, string containerName)
+        {
+            CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+            return cloudBlobClient.GetContainerReference(containerName);
+        }
+    }
+}
--- src/Compact.Functions/RouteProcessor.cs
+++ src/Compact.Functions/RouteProcessor.cs
@@ -1,23 +1,35 @@
 using Compact.Models;
+using HtmlAgilityPack;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.IO;
-using System.Linq;
+using System.Threading.Tasks;
 
 namespace Compact.Functions
 {
     public static class RouteProcessor
     {
         [FunctionName("NewRouteProcessor")]
-        public static void Run([BlobTrigger("tests/{name}", Connection = "StorageConnectionString")] Stream routeStream, string name, ILogger log)
+        public static async Task RunAsync([BlobTrigger("tests/{name}", Connection = "StorageConnectionString")] Stream routeStream, string name, ILogger log)
         {
+            var route = GetRoute(routeStream);
             log.LogInformation($"Detected new Route: {name}");
 
-            var route = GetRoute(routeStream);
+            foreach (var link in route.Links)
+            {
+                // We don't need to process a file which has already been processed
+                if (!string.IsNullOrEmpty(link.Title))
+                {
+                    return;
+                }
 
-            log.LogInformation(route.Id);
-            log.LogInformation(route.Links.First().Target);
+                SourceLinkMetadata(link);
+                log.LogInformation($"Appended title: {link.Title} to link: {link.Target}");
+            }
+
+            await UpdateRouteFileAsync(name, route);
+            log.LogInformation($"Route processed: {route.Id}");
         }
 
         private static RouteModel GetRoute(Stream routeStream)
@@ -29,6 +41,23 @@ namespace Compact.Functions
             var result = JsonConvert.DeserializeObject<RouteModel>(routeContent);
 
             return result;
+        }
+
+        private static void SourceLinkMetadata(LinkModel link)
+        {
+            var webGet = new HtmlWeb();
+            var document = webGet.Load(link.Target);
+
+            link.Title = document.DocumentNode.SelectSingleNode("html/head/title").InnerText;
+        }
+
+        private static async Task UpdateRouteFileAsync(string name, RouteModel route)
+        {
+            var storageConnectionString = System.Environment.GetEnvironmentVariable("StorageConnectionString");
+
+            var azureStorageManager = new AzureStorageManager(storageConnectionString);
+
+            await azureStorageManager.StoreObject("tests", name, route);
         }
     }
 }
